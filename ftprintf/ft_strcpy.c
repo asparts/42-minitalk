bc20815c2f6c2c521ea036372ffb9bb146207de3
markus <nummimarkus@gmail.com>
2023-11-23 06:06:41 +0200

    reimported ftprintf so the code is little bit changed and guaranteed to work

--- ftprintf/ft_strcpy.c
+++ ftprintf/ft_strcpy.c
@@ -6,25 +6,24 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 00:41:57 by mnummi            #+#    #+#             */
-/*   Updated: 2023/07/27 08:42:43 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/11/07 20:37:23 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char    *ft_strcpy(char *dest, const char *src)
+char	*ft_strcpy(char *dest, const char *src)
 {
-        int     i;
+	int	i;
 
-        i = 0;
-        if (!dest)
-                return (NULL);
-        while (src[i] != '\0')
-        {
-                dest[i] = src[i];
-                i++;
-        }
-        dest[i] = '\0';
-        return (dest);
+	i = 0;
+	if (!dest)
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
-
